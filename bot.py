b40ace73987aa592134c7addae8b7f4619ecec4a
Steven Young <steven97@me.com>
2016-09-24 18:15:40 -0400

    Added comments

--- bot.py
+++ bot.py
@@ -6,29 +6,18 @@ from random import shuffle
 # Create the client object
 client = discord.Client()
 
-# Make the empty lists all in one place (because its the right thing to do)
-entered = []
-captains = []
-team1 = []
-team2 = []
-picking = False
-
 # Set the things from the config file
 admins = config.admins
 cmdprefix = config.cmdprefix
 teamsize = config.teamsize
 pugsize = config.pugsize
 
+picking = False
+
 # When the bot recieves a message
-
-
 @client.event
 async def on_message(msg):
     global picking
-    global entered
-    global captains
-    global team1
-    global team2
 
     # Don't reply to self
     if msg.author == client.user:
@@ -49,17 +38,22 @@ async def on_message(msg):
 
     # Add
     if (msg.content.startswith(cmdprefix + "add")):
+
+        # If in the picking phase, don't allow !add
         if (picking):
             await client.send_message(msg.channel, "You cannot use this command in the picking phase")
+
+        # If the user is already in the queue, tell them
         elif (msg.author in entered):
             await client.send_message(msg.channel, "You are already in the queue")
+
+        # If the pug is full lets get this party started
         elif (len(entered) == (pugsize - 1)):
-            # elif (len(entered) == 2):
             entered.append(msg.author)
 
-            # start the pug
             picking = True
 
+            # Set the captains
             shuffle(entered)
             captains = [entered[0], entered[1]]
             team1 = [captains[0]]
@@ -67,17 +61,20 @@ async def on_message(msg):
             entered.remove(team1[0])
             entered.remove(team2[0])
 
+            # Send the starting message
             startingMsg = "PUG Starting!\nCaptains are " + captains[0].mention + " and " + captains[1].mention + \
                 "\n" + captains[0].mention + " will have first pick"
             await client.send_message(msg.channel, startingMsg)
 
-            # while teams are not full
+            # While teams are not full let captains take turns picking players
             while(len(team1) < teamsize and len(team2) < teamsize):
 
                 async def team1func(msg):
                     inputobj = 0
                     await client.send_message(msg.channel, captains[0].mention + " Type @player to pick. Available players are:\n" + ("\n".join(map(str, entered))))
 
+                    # This block of code checks for a pick from captain1 and
+                    # catches the exception if they send a message that doesn't contain a mention
                     while True:
                         try:
                             inputobj = await client.wait_for_message(author=msg.server.get_member(captains[0].id))
@@ -86,23 +83,28 @@ async def on_message(msg):
                             continue
                         break
 
+                    # If the pick isn't on a team, add to team1
                     if(team1add in entered and team1add not in team1 and team1add not in team2):
                         team1.append(team1add)
                         entered.remove(team1add)
                         await client.send_message(msg.channel, team1add.mention + " Added to your team")
 
+                    # If the pick is already on a team, tell captain1 and let them pick again
                     elif(team1add in entered and team1add in team1 or team1add in team2):
                         await client.send_message(msg.channel, team1add.mention + " Is already on a team")
                         await team1func(msg)
 
+                    # If the pick isn't in the queue, tell captain1 and let them pick again
                     elif(team1add not in entered):
                         await client.send_message(msg.channel, team1add.mention + " Is not in the queue")
                         await team1func(msg)
 
+                    # This probably shouldn't even be here, but whatever
                     else:
                         await client.send_message(msg.channel, "Unknown error")
                         await team1func(msg)
 
+                # Same as above, but for team2
                 async def team2func(msg):
                     inputobj = 0
                     await client.send_message(msg.channel, captains[1].mention + " Type @player to pick. Available players are:\n" + ("\n".join(map(str, entered))))
@@ -135,6 +137,7 @@ async def on_message(msg):
                 await team1func(msg)
                 await team2func(msg)
 
+            # Setup a way to mention the teams
             team1mention = []
             team2mention = []
             for i in team1:
@@ -142,8 +145,10 @@ async def on_message(msg):
             for i in team2:
                 team2mention.append(i.mention)
 
+            # Send a message containg the teams, GLHF!
             await client.send_message(msg.channel, "Team 1 is: " + "\n".join(map(str, team1mention)) + "\nTeam2 is: " + "\n".join(map(str, team2mention)) + "\n GLHF!")
 
+            # Reset to allow for anotherone
             entered = []
             captains = []
             team1 = []
@@ -152,6 +157,7 @@ async def on_message(msg):
             team2mention = []
             picking = False
 
+        # Add player to queue if the queue isn't full
         else:
             entered.append(msg.author)
             await client.send_message(msg.channel, msg.author.mention + " Successfuly entered into the queue. " + str(len(entered)) + " Players in queue")
@@ -180,6 +186,14 @@ async def on_message(msg):
             await client.send_message(msg.channel, "Queue reset")
         else:
             await client.send_message(msg.channel, "You do not have access to this command")
+
+    # Easter egg 1
+    if ("Kappa" in msg.content):
+        await client.send_message(msg.channel, "http://i.imgur.com/cpzYXCI.png")
+
+    # Easter egg 2
+    if (msg.content.startswith(cmdprefix + "egg")):
+        await client.send_message(msg.channel, "http://i.imgur.com/GZ79poy.jpg")
 
 
 # Print when ready
